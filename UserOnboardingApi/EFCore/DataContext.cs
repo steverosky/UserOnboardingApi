29e19f3bf42adbbc204768c1db4f640b3ee17d68
Steve Ofori <oforistep@gmail.com>
2022-11-12 21:45:22 +0000

    Add project files.

---
+++ UserOnboardingApi/EFCore/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserOnboardingApi.EFCore
+{
+    public class EF_DataContext : DbContext
+    {
+        public EF_DataContext(DbContextOptions<EF_DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+   
+}
---
+++ UserOnboardingApi/EFCore/Users.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserOnboardingApi.EFCore
+{
+    [Table("users")]
+    public class User
+    {
+        [Key, Required]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
